e6bfa5b7484eddf7c316c8478666daf94b63f0bf
shimamura <14ec063@ms.dendai.ac.jp>
2016-10-09 23:16:23 +0900

    phpでなびべーしょんばー

---
+++ main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="SHIFT-JIS">
+ <link REL="stylesheet" type="text/css" HREF="css/menu.css">
+<title>どこかのPage</title>
+</head>
+
+<body>
+<div id="menu">
+<?php
+/* 広告があれば表示する */
+@include("menu.html");
+?>
+</div>
+<div >&nbsp;</div>
+<br clear="left">
+<div id="middle" class="item" clear="left">
+	<div id="left_frame" class="item">
+<h1>Title</h1>
+<hr />
+<p>初めていらした方はこちらをお読みください&darr;<br /> <a href="warn.html" >注意事項</a></p>
+<h2>更新履歴[最新5項目]</h2>
+
+<p>2016/08/21_資料集移転＋微修正</p>
+
+<hr />
+		<div class="spacer"></div>
+	</div>
+	<div id="right_frame" class="item">
+<h1>Site</h1>
+<hr />
+<h2>■ COUNTER</h2>
+<div id="count">
+<!-- FC2カウンター ここから -->
+<script language="javascript" src="http://counter1.fc2.com/counter.php?id=31624625&amp;main=1"></script>
+<noscript><img src="http://counter1.fc2.com/counter_img.php?id=31624625&main=1" /></noscript><!-- FC2カウンター ここまで --></div>
+<h2>■ ABOUT</h2>
+<p>サイトです。</p>
+<h2>■ ABOUT LINK</h2>
+<p><img src="img/banner1.png" height="40" width="200" /><br /> 【サイト名】 <br /> 【管理人】 倉沢<br /> 【HPアドレス】<br />&nbsp; htt<br /> freespase</p>
+<h2>■ 説明</h2>
+<p><a href="chara.html" >説明ページへ</a></p>
+<h2>■ 画面上部のメニューが表示されない方へ</h2>
+<p>こちらからお入りください。&darr;<br /> <a href="map.html" >サイトマップを表示する</a></p>
+<h2>■ 拍手</h2>
+<a href="http://clap.webclap.com/clap.php?id=norasawa" target="_blank" onclick="window.open('http://clap.webclap.com/clap.php?id=norasawa','webclap','toolbar=no,location=no,directories=no,status=no,scrollbars=yes,resizable=yes');return false;"><img src="http://img.webclap.com/webclap/button/off.php?img=32" style="border: 0px;" /></a>
+		<div class="spacer2"></div>
+	</div>
+</div>
+</body>
+</html>
